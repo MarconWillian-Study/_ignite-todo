1b58bfeab95a7599016887c7a3fda5d2e6297c46
Marcon Willian <oi@marconwillian.dev>
2021-04-25 01:01:05 -0300

    chore ✨: Add create todo

--- src/controller/todo/index.ts
+++ src/controller/todo/index.ts
@@ -1,9 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import * as uuid from 'uuid';
+import { query } from 'faunadb';
+import { fauna } from '../../services/fauna'
+
+interface CreateInput {
+  title: string
+}
+
 const todo = {
   index(req, res){
     res.status(200).json({ name: 'list todos' })
   },
-  create(req, res){
-    res.status(200).json({ name: 'create todo' })
+  async create(request: NextApiRequest, response: NextApiResponse){
+    const { title } = request.body as CreateInput;
+
+    const todo = { 
+      id: uuid.v4(),
+      user: 1,
+      title,
+      status: false
+    };
+
+    await fauna.query(
+      query.Create(
+        query.Collection('todos'),
+        { 
+          data: todo
+        }
+      ),
+    );
+
+    response.status(200).json(todo)
   },
   get(req, res){
     res.status(200).json({ name: 'get todo' })
