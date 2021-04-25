a8a896cec1d58ca40e8fba9db332b98c282e8a55
Marcon Willian <oi@marconwillian.dev>
2021-04-25 11:43:29 -0300

    chore ✨: Can update a status

--- src/controller/todo/status.ts
+++ src/controller/todo/status.ts
@@ -1,6 +1,38 @@
+import { query } from "faunadb"
+import { NextApiRequest, NextApiResponse } from "next"
+import { fauna } from "../../services/fauna"
+
 const status = {
-  update(req, res){
-    res.status(200).json({ name: 'update status' })
+  async update(request: NextApiRequest, response: NextApiResponse, status: boolean){
+    const { id } = request.query
+
+    try {
+      await fauna.query(
+        query.Update(
+          query.Select(
+            "ref",
+            query.Get(
+              query.Match(
+                query.Index('todo_by_id'),
+                query.Casefold(id)
+              )
+            )
+          ),
+          {
+            data: { 
+              status
+            }
+          }
+        ),
+      )
+  
+      response.status(201).json({ id, status});
+    } catch (error) {
+      response.status(400).json({
+        error: true, 
+        message: error.message
+      })
+    }
   }
 }
 
